03594f4495bee8ae2999033d1f491406f462c6b2
Marcos Lilljedahl <marcosnils@gmail.com>
2026-01-31 01:36:05 -0300

    chan!
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ type CheckStatus struct{}
 func (m *ChecksTest) CheckMatias(
 	ctx context.Context,
 ) *CheckStatus {
-	// abcdefghijklmno
+	// a
 	if m.Secret != nil {
 		fmt.Println(m.Secret.Plaintext(ctx))
 	} else {
